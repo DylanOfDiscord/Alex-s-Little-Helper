86809e5f5119d191fae22cc838c8a26e71ba91c4
DylanOfDiscord <72952471+DylanOfDiscord@users.noreply.github.com>
2020-11-26 23:52:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "YOUR_DISCORD_BOT_TOKEN",
-    channel: "XXXXXXXXXXXXXXXXXX",
+    token: "NzgxNzQyNjE1NTY5MjM1OTY5.X8CEiw.MayiXZABRvMfsP9Q_nrKhOyHC9Q",
+    channel: "770693443286597702",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "YOUTUBE_CHANNEL_ID",
+    channel_id: "UC1coEm15I15s6RnbH_IzT5A",
     watchInterval: 30000
 }
